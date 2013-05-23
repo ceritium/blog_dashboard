27250da6f21fd26d4c093d17f3293a19730dcceb
José Galisteo <ceritium@gmail.com>
2013-05-23 10:31:31 +0200

    moved redactor to dummy app

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 BlogDashboard::Engine.routes.draw do
 
-  mount RedactorRails::Engine => '/redactor_rails'
 
   match "/" => "posts#index", as: :blog_dashboard_root
   resources :posts
--- test/dummy/config/routes.rb
+++ test/dummy/config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
 
   devise_for :users
   mount BlogDashboard::Engine => "/admin"
+  mount RedactorRails::Engine => '/redactor_rails'
   resources :posts
 end
